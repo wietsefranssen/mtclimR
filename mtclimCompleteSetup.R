7e3fcaef763e080af6c03236c0aec172053260f3
Wietse Franssen <wietse.franssen@wur.nl>
2017-08-08 11:48:18 +0200

    little cleanup

--- mtclimCompleteSetup.R
+++ mtclimCompleteSetup.R
@@ -5,7 +5,6 @@ rm (list = ls())
 library(WFRTools)
 library(doParallel)
 library(mtclimR)
-# library(pbdNCDF4)
 nCores<-2
 registerDoParallel(cores=nCores)
 start.time.total <- Sys.time()
@@ -15,29 +14,18 @@ print(paste("nCores: ", nCores))
 settings <- initSettings(startdate = "1950-01-01",
                          enddate = "1950-1-31",
                          outstep = 6,
-#                         lonlatbox = c(100.75, 102.25, 32.25, 36.25))
+                        lonlatbox = c(100.75, 102.25, 32.25, 36.25))
 # lonlatbox = c(92.25, 110.25, 7.25, 36.25))
-lonlatbox = c(-179.75, 179.75, -89.75, 89.75))
+# lonlatbox = c(-179.75, 179.75, -89.75, 89.75))
 
 ## INIT INPUT FILES/VARS
 settings <- setInputVars(settings,list(
-  pr         = list(ncFileName = "/home/wietse/Documents/Projects/VIC_model/MetSim/dataWietse/pr_bced_1960_1999_gfdl-esm2m_hist_1950.nc",        ncName = "prAdjust",        alma = TRUE, vicIndex = 9),
-  tasmin     = list(ncFileName = "/home/wietse/Documents/Projects/VIC_model/MetSim/dataWietse/tasmin_bced_1960_1999_gfdl-esm2m_hist_1950.nc",        ncName = "tasminAdjust",     vicIndex = 17),
-  tasmax     = list(ncFileName = "/home/wietse/Documents/Projects/VIC_model/MetSim/dataWietse/tasmax_bced_1960_1999_gfdl-esm2m_hist_1950.nc",        ncName = "tasmaxAdjust",     vicIndex = 16),
-  wind       = list(ncFileName = "/home/wietse/Documents/Projects/VIC_model/MetSim/dataWietse/wind_bced_1960_1999_gfdl-esm2m_hist_1950.nc",        ncName = "windAdjust",       vicIndex = 20)
-  # shortwave  = list(ncFileName = "/home/wietse/Documents/Projects/VIC_model/MetSim/dataWietse",        ncName = "tasmaxAdjust", vicIndex = 14),
-  # longwave   = list(ncFileName = "/home/wietse/Documents/Projects/VIC_model/MetSim/dataWietse",        ncName = "tasmaxAdjust",  vicIndex = 6)
+  pr         = list(ncFileName = "./data/merged_Mekong.nc",        ncName = "prAdjust",        vicIndex = 9,   alma = FALSE),
+  tasmin     = list(ncFileName = "./data/merged_Mekong.nc",        ncName = "tasminAdjust",    vicIndex = 17),
+  tasmax     = list(ncFileName = "./data/merged_Mekong.nc",        ncName = "tasmaxAdjust",    vicIndex = 16),
+  wind       = list(ncFileName = "./data/merged_Mekong.nc",        ncName = "windAdjust",      vicIndex = 20)
 ))
-settings$elevation  <- list(ncFileName = "/home/wietse/Documents/Projects/VIC_model/MetSim/dataWietse/WFDEI-elevation.nc", ncName = "elevation")
-
-## INIT INPUT FILES/VARS
-# settings <- setInputVars(settings,list(
-#   pr         = list(ncFileName = "./data/merged_Mekong.nc",        ncName = "prAdjust",        vicIndex = 9,   alma = FALSE),
-#   tasmin     = list(ncFileName = "./data/merged_Mekong.nc",        ncName = "tasminAdjust",    vicIndex = 17),
-#   tasmax     = list(ncFileName = "./data/merged_Mekong.nc",        ncName = "tasmaxAdjust",    vicIndex = 16),
-#   wind       = list(ncFileName = "./data/merged_Mekong.nc",        ncName = "windAdjust",      vicIndex = 20)
-# ))
-# settings$elevation <- list(ncFileName = "./data/domain_elev_Mekong.nc", ncName = "elev")
+settings$elevation <- list(ncFileName = "./data/domain_elev_Mekong.nc", ncName = "elev")
 
 ## INIT OUTPUT FILES/VARS
 settings$outputVars <- list(
@@ -48,8 +36,6 @@ settings$outputVars <- list(
   pressure     = list(ncName = "pressure"),
   wind       = list(ncName = "wind")
 )
-
-### THE MAIN ROUTINE
 
 ## LOAD MASK/ELEVATION
 elevation <- ncLoad(file = settings$elevation$ncFileName,
@@ -63,6 +49,7 @@ makeNetcdfOut(settings, elevation)
 el <- array(NA, dim = c(length(elevation$xyCoords$x), length(elevation$xyCoords$y), settings$intern$nrec_out))
 toNetCDFData <- list(el)[rep(1,length(settings$outputVars))]
 print(paste0("output array: ", format(object.size(toNetCDFData), units = "auto")))
+rm(el)
 
 ## LOAD WHOLE DOMAIN FROM NETCDF
 print(paste0("reading..."))
@@ -71,9 +58,9 @@ forcing_dataRTotal <- readForcingAll(settings, elevation)
 print(paste0("forcing array: ", format(object.size(forcing_dataRTotal), units = "auto")))
 print(sprintf("read: %6.1f min",as.numeric(Sys.time() - start.time.read, units = "mins")))
 
-start.time.mtclim <- Sys.time()
 ## CELL LOOP
 for (iy in 1:length(elevation$xyCoords$y)) {
+  start.time.mtclim <- Sys.time()
   print(paste("running iy: ",iy))
   output<-foreach(ix = 1:length(elevation$xyCoords$x)) %dopar% {
     # for (ix in 1:length(elevation$xyCoords$x)) {
@@ -91,21 +78,19 @@ for (iy in 1:length(elevation$xyCoords$y)) {
   }
   print(paste0("mtclim temp array: ", format(object.size(output), units = "auto")))
   print(sprintf("mtclim: %6.1f min",as.numeric(Sys.time() - start.time.mtclim, units = "mins")))
-  print(paste0("adding to output array..."))
-  iix <- 1
-  print(paste0("llll: " ,length(output)))
+
   for (ix in 1:length(elevation$xyCoords$x)) {
-  # print(paste("ix: ", ix))
     if (!is.na(elevation$Data[iy,ix])) {
-      # print(paste("ixXX: ", ix))
       ## ADD TO OUTPUT ARRAY
-      for (iVar in 1:length(settings$outputVars)) toNetCDFData[[iVar]][ix,iy,] <- output[[ix]][[iVar]]
-      iix <- iix + 1
+      for (iVar in 1:length(settings$outputVars)) {
+#      if (!is.na(output[[ix]][[iVar]][1])) {
+        toNetCDFData[[iVar]][ix,iy,] <- output[[ix]][[iVar]]
+#  }
+      }
     }
   }
-  # rm(output)
+  rm(output)
   print(paste0("adding to output array: done"))
-  # print(paste(iy,outputt))
 }
 
 ## ADD OUTPUT TO NETCDF
@@ -126,7 +111,6 @@ for (iVar in 1:length(settings$outputVars))
   )
 }
 nc_close(ncid)
+
 print(sprintf("write: %6.1f min",as.numeric(Sys.time() - start.time.write, units = "mins")))
 print(sprintf("total: %6.1f min",as.numeric(Sys.time() - start.time.total, units = "mins")))
-
-
